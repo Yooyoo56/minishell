c0b24abca2e522351121371d212fdc2c786e0c84
Yoojeong TAK <ytak@e1r6p8.42.fr>
2021-12-17 19:45:47 +0100

    echo done

--- src/cmds/ft_cmds_echo.c
+++ src/cmds/ft_cmds_echo.c
@@ -6,7 +6,7 @@
 /*   By: ytak <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 15:08:33 by ytak              #+#    #+#             */
-/*   Updated: 2021/12/17 16:44:42 by ytak             ###   ########.fr       */
+/*   Updated: 2021/12/17 18:18:53 by ytak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,21 @@ void	ft_echo(t_cmd *cmd)
 	int	i;
 
 	i = 0;
-    // if there is one argument, no space !
-    // printf("%s", cmd->args[i++]);
-	///if there - => print - argument
-	if (cmd->args)
-		while (cmd->args[i])
-			printf("%s ", cmd->args[i++]);
+	if (cmd->flag != NULL && ft_strncmp(cmd->flag, "-n", ft_strlen(cmd->flag) + 2) != 0)
+	{
+		printf("%s", cmd->flag);
+		if (cmd->args[0])
+			printf(" ");
+	}
+	while (cmd->args[i])
+	{
+		printf("%s", cmd->args[i]);
+		if (cmd->args[i + 1] != NULL)
+			printf(" ");
+		i++;
+	}
 	if (cmd->flag == NULL)
 		printf("\n");
-	else if (ft_strncmp(cmd->flag, "-n", ft_strlen(cmd->flag)) != 0)
+	else if (ft_strncmp(cmd->flag, "-n", ft_strlen(cmd->flag) + 2) != 0)
 		printf("\n");
 }
